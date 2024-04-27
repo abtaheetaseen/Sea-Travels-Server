f329e5b538ff933c80c6579324c2c44310e9d43d
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-04-27 10:16:16 +0600

    fourth commit

--- index.js
+++ index.js
@@ -43,7 +43,11 @@ async function run() {
         res.send(result);
     })
 
-
+    // get by email
+    app.get("/touristSpots/:email", async(req, res) => {
+        const result = await touristSpotsCollection.find({email: req.params.email}).toArray()
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
